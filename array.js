a87ec5ec0c1070682cfced985440a83b02c3a17f
siam114 <smsiamislam598@gmail.com>
2025-03-10 21:26:58 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,27 @@
+const arr = [0, 1, 2, 3, 4, 5]
+// console.log(arr[4])
+// arr.push(6)
+// arr.pop()
+// arr.unshift(9)
+// arr.shift()
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(4))
+// console.log(arr.indexOf(9))
+
+// const newArr = arr.join()
+// console.log(newArr)
+// console.log(arr)
+
+const marbel_heros = ['thor','ironman','spiderman']
+const dc_heros=['superman','flash','batman']
+// marbel_heros.push(dc_heros)
+// console.log(marbel_heros)
+
+// const allHeros = marbel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+// const all_new_heros = [...marbel_heros, ...dc_heros]
+// console.log(all_new_heros)
+
+console.log(Array.isArray('siam'))
+console.log(Array.from('siam'))
---
+++ service.js
@@ -0,0 +1,31 @@
+// console.log("siam")
+const siam = 123;
+let s = "siam";
+var k= "islam"
+// console.table([siam,s,k]);
+// console.log(typeof s)
+// console.log(typeof siam)
+let val= 3
+// let negval=-val
+// console.log(negval)
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+let str1 = "SM Siam"
+let str2 = " islam"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("2" === 1)
+// console.log("02" > 1)
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)

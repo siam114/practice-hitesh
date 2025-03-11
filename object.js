7e1d4dc3e58ab60ec13813bb70d39b876dafdc6c
siam114 <smsiamislam598@gmail.com>
2025-03-11 11:07:30 +0600

    object part 2

--- object.js
+++ object.js
@@ -16,3 +16,30 @@ user.email = 'siamislam@gmail.com'
 // Object.freeze(user)
 user.email = 'siam@gmail.com'
 // console.log(user)
+
+const obj1 = {1: 'a', 2: 'b'}
+const obj2 = {3: 'a', 4: 'b'}
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1,...obj2}
+// console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        name: 'siam'
+    },
+    {
+        id: 2,
+        name: 'islam'
+    },
+    {
+        id: 3,
+        name: 'sm siam'
+    }
+]
+
+// console.log(users[1].id)
+// console.log(obj3)
+// console.log(Object.keys(obj3))
+// console.log(Object.values(obj3))
+// console.log(Object.entries(obj3))

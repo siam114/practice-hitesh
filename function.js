fef5d03f96fdc2a1d7061b3a50e86974dd85efe2
siam114 <smsiamislam598@gmail.com>
2025-03-11 11:39:04 +0600

    function create

---
+++ function.js
@@ -0,0 +1,21 @@
+ function myname (){
+    console.log('s')
+    console.log('i')
+    console.log('a')
+    console.log('m')
+ }
+//  myname()
+// function addTwoNum (num1,num2){
+//     console.log(num1 + num2)
+// }
+// addTwoNum(3,4)
+// addTwoNum(3,'4')
+// addTwoNum(3,null)
+
+function addTwoNumber (num1,num2) {
+    let result = num1 + num2;
+    return result
+}
+
+const result = addTwoNumber(3,5)
+console.log(result)

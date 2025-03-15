968c7a2b0255d28bd5873cf5346fd58bfc5a0892
siam114 <smsiamislam598@gmail.com>
2025-03-16 00:19:16 +0600

    explore condition if es;e id

--- condition.js
+++ condition.js
@@ -4,3 +4,13 @@ if(temperature <= 50){
 }else{
     console.log('temperature is greater then 50')
 }
+
+const balance = 1000;
+
+if(balance < 500){
+    console.log('less then 500')
+}else if(balance < 750){
+    console.log('less then 750')
+}else{
+    console.log('lesss then 1200');
+}

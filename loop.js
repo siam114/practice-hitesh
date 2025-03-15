c7c15daff426544c2c32cb110b2e8d4b9a9225e2
siam114 <smsiamislam598@gmail.com>
2025-03-16 00:54:13 +0600

    nasted loop

--- loop.js
+++ loop.js
@@ -1,6 +1,17 @@
 let array = [1,2,3,4,5,6]
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    console.log(element)
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if(element == 5){
+        // console.log('5 is the best number')
+    }
+    // console.log(element)
     
 }
+
+
+// for(let i=0; i<=10; i++){
+//     console.log(`Outer loop value: ${i}`)
+//     for(j=0; j<=10; j++){
+//         console.log(`Inner loop value: ${j} and inner loop: ${i}`)
+//     }
+// }

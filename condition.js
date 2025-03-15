d33320392325eb649b8ef2d9690553696b69edb0
siam114 <smsiamislam598@gmail.com>
2025-03-16 00:30:10 +0600

    explore switch

--- condition.js
+++ condition.js
@@ -12,5 +12,59 @@ if(balance < 500){
 }else if(balance < 750){
     console.log('less then 750')
 }else{
-    console.log('lesss then 1200');
+    console.log('less then 1200');
+}
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch(month){
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    case 4:
+        console.log('April');
+        break;
+    case 5:
+        console.log('May');
+        break;
+    case 6:
+        console.log('June');
+        break;
+    case 7:
+        console.log('July');
+        break;
+    case 8:
+        console.log('Aug');
+        break;
+    case 9:
+        console.log('Septembar');
+        break;
+    case 10:
+        console.log('Oct');
+        break;
+    case 11:
+        console.log('Nov');
+        break;
+    case 12:
+        console.log('Dec');
+        break;
+
+    default:
+        console.log('default case')
+        break;
 }

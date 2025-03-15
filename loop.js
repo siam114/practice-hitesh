59ca42e85d8e6355868b8b5cb675bb79c044ea27
siam114 <smsiamislam598@gmail.com>
2025-03-16 01:03:19 +0600

    continue and break

--- loop.js
+++ loop.js
@@ -23,7 +23,15 @@ for(let i=1; i<=10; i++){
     console.log(`Outer loop value: ${i}`)
     for(j=1; j<=10; j++){
         // console.log(`Inner loop value: ${j} and inner loop: ${i}`)
-        // console.log(i + '*' + j + '=' + i*j)
+        console.log(i + '*' + j + '=' + i*j)
         
     }
+}
+
+for (let index = 0; index < 10; index++) {
+    if(index == 5){
+        console.log('detected 5')
+        continue
+    }
+    console.log(`value of i is ${index}`)
 }

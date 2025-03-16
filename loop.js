929d096c5420c793f0ffc1d5889f71aa4cab840e
siam114 <smsiamislam598@gmail.com>
2025-03-16 14:24:38 +0600

    do while loop

--- loop.js
+++ loop.js
@@ -37,8 +37,15 @@ for (let index = 0; index < 10; index++) {
 }
 
 
-let index=0
-while(index <=10){
-    console.log(`Value of index is ${index}`)
-    index = index +2
-}
+// let index=0
+// while(index <=10){
+//     console.log(`Value of index is ${index}`)
+//     index = index +2
+// }
+
+
+let score = 1;
+do{
+    console.log(`Score is ${score}`)
+    score++
+}while(score <= 10)
